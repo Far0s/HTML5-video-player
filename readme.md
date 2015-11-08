ee417c70ef00c13d116bea83630d5393580086e8
Sebastien Dancer-Michel <s.dancermichel@gmail.com>
2015-11-08 01:17:14 +0100

    Sunday commit

--- readme.md
+++ readme.md
@@ -1,10 +1,30 @@
-# H2 P2019 - Developpement Web
+# HTML5 Video Player
 
-## HTML5 Video Player
+## H2 P2019 - Web development
+The goal is to make a video player in ~1 week, with the classic characteristics of HTML5 video controls (play/pause, volume control, fullscreen, etc.) plus any bonus functionality I can think of.
+The player must be compatible with every HTML5-ready web browser.
 
+## My take on the subject
+I tried to make a player as simple as possible, with self-explanatory code and
+discreet animations.  
+
+## Features
+- Controls bar: appears by default, when the video plays is disappears and reappear on video hovering.
+- Play/pause button + clicking anywhere on the video triggers the same function as clicking on the button
+- Volume button: clicking mutes/unmutes the video
+- Volume slider: more precise control over volume.
+- Fullscreen button: Sets the player fullscreen, can't do a more obvious description
+---
+Roadmap :
+- Have custom controls on fullscreen, instead of default browser controls
+- Tune the timeline to have a cursor and some "wave" animation going on
+- Keyboard shortcuts : spacebar for play/pause, f for fs, 1-9 for fast-forward shortcuts, etc.
 
 
 ### Inspiration :
+- [Youtube Play/Pause Button animation](http://codepen.io/aralon/pen/NqGWXZ)
+- [Building custom controls for HTML5 videos](http://blog.teamtreehouse.com/building-custom-controls-for-html5-videos)
+- [Codepen - VolumeSlider by Robert Pataki](http://codepen.io/heartcode/pen/sywuk)
 
-- Youtube Play/Pause Button animation : http://codepen.io/aralon/pen/NqGWXZ
-- Building custom controls for HTML5 videos : http://blog.teamtreehouse.com/building-custom-controls-for-html5-videos
+### Rights : 
+The video & song used for this demo is [Cute Kittens Fly in Slow Motion to Hip Hop Dubstep - by ScottDW](https://www.youtube.com/watch?v=3oem-M2tQU4). All rights belong to them.
--- script.js
+++ script.js
@@ -1,38 +1,16 @@
+// SETUP
+
 var player = {};
 
-// SETUP
 player.container = document.querySelector('.player');
 player.video = player.container.querySelector('video');
-// player.volume_up = player.container.querySelector('.volume-up');
-// player.volume_down = player.container.querySelector('.volume-down');
 player.timeline = player.container.querySelector('.timeline');
 player.timeline_elapsed = player.container.querySelector('.timeline-elapsed');
 player.btn_playpause = player.container.querySelector('.btn-playpause');
 player.btn_volume = player.container.querySelector('.btn-volume');
+player.volume_slider = player.container.querySelector('.volume-slider');
+player.volume_level = player.container.querySelector('.volume-level');
 player.btn_fullscreen = player.container.querySelector('.btn-fullscreen');
-
-
-// VOLUME 
-// player.volume_up.addEventListener('click', function(){
-//   var volume = player.video.volume + 0.1;
-//   if (volume > 1) volume = 1;
-//   player.video.volume = volume;
-// });
-// player.volume_down.addEventListener('click', function(){
-//   var volume = player.video.volume - 0.1;
-//   if (volume < 0) volume = 0;
-//   player.video.volume = volume;
-// });
-
-player.btn_volume.addEventListener('click', function(){
-  if (player.video.muted == false) {
-    player.video.muted = true;
-    console.log('muted');
-  } else {
-    player.video.muted = false;
-    console.log('unmuted');
-  }
-});
 
 
 // TIMELINE
@@ -43,21 +21,23 @@ window.setInterval(function(){
 
 player.timeline.addEventListener('click', function(e){
   var bounding_rect = player.timeline.getBoundingClientRect(),
-      x = e.clientX - bounding_rect.left,
-      ratio = x / bounding_rect.width,
-      time = ratio * player.video.duration;  
+  x = e.clientX - bounding_rect.left,
+  ratio = x / bounding_rect.width,
+  time = ratio * player.video.duration;  
   player.video.currentTime = time;
 });
 
 // PLAY PAUSE
-
+player.container.classList.add('paused');
 var playSwitch = function(){
   if (player.video.paused){
     player.video.play();
     player.btn_playpause.classList.remove('paused');
+    player.container.classList.remove('paused');
   } else {
     player.video.pause();
     player.btn_playpause.classList.add('paused');
+    player.container.classList.add('paused');
   }
 };
 
@@ -70,11 +50,8 @@ player.btn_playpause.addEventListener('click', function(){
 });
 
 
-
-
-
 // FULLSCREEN
-player.btn_fullscreen.addEventListener("click", function() {
+function goFS(){
   if (player.video.requestFullscreen) {
     player.video.requestFullscreen();
   } else if (player.video.mozRequestFullScreen) {
@@ -82,5 +59,42 @@ player.btn_fullscreen.addEventListener("click", function() {
   } else if (player.video.webkitRequestFullscreen) {
     player.video.webkitRequestFullscreen(); // Chrome and Safari
   }
+}
+
+player.btn_fullscreen.addEventListener("click", function() {
+  goFS();
 });
+player.video.addEventListener('dblclick', function(){
+  goFS();
+});
+
+
+
+// VOLUME 
+player.volume_level.style.transform = 'scaleX('+ 1 +')'; // sets the volume slider at 1
+function setVolume(e){
+  var bounding_rect = player.volume_slider.getBoundingClientRect(),
+  x = e.clientX - bounding_rect.left,
+  ratio = x / bounding_rect.width;
+  player.volume_level.style.transform = 'scaleX('+ ratio +')';
+  player.video.volume = ratio;
+}
+
+player.btn_volume.addEventListener('click', function(){
+  if (player.video.muted == false) {
+    player.video.muted = true
+    player.btn_volume.classList.add('mute');
+  } else {
+    player.video.muted = false;
+    player.btn_volume.classList.remove('mute');
+  }
+});
+
+
+player.volume_slider.addEventListener('click', function(e){
+  setVolume(e);
+});
+
+
+
 
